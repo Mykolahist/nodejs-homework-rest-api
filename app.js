b4bb265360d7bf8e71054bc6ddab2d1c1282ecc3
Mykolahist <71245049+Mykolahist@users.noreply.github.com>
2021-11-22 01:20:27 +0200

    create branch

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
-const contactsRouter = require('./routes/api/contacts')
+// const contactsRouter = require('./routes/api/contacts')
 
 const app = express()
 
@@ -10,15 +10,23 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
 app.use(cors())
-app.use(express.json())
+// app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.get("/contacts", (req, res) => {
+  res.json({
+    data: {
+      result: "contacts"
+    }
+  })
+})
 
-app.use((req, res) => {
+// app.use('/api/contacts', contactsRouter)
+
+app.use((_, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
+app.use((err, _, res, __) => {
   res.status(500).json({ message: err.message })
 })
 
