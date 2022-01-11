ed55fcd2580dbde7fd19becdadb1f22acaea6f7c
Mykolahist <71245049+Mykolahist@users.noreply.github.com>
2022-01-11 10:41:33 +0200

    delete extra files

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,13 +1,9 @@
 const signup = require('./signup')
 const signin = require('./signin')
 const signout = require('./signout')
-const verify = require('./verify')
-const resendEmail = require('./resendEmail')
 
 module.exports = {
   signup,
   signin,
   signout,
-  verify,
-  resendEmail,
 }
